4503f0b307e8bcc355e4d012c98a906ad553d316
-g <-g>
2020-07-08 21:19:52 -0400

    one away and string compression

--- src/main/java/arraystring/_01_05_OneAway.java
+++ src/main/java/arraystring/_01_05_OneAway.java
@@ -10,11 +10,29 @@ package arraystring;
  * pales, pale -> true
  * pale, bale -> true
  * pale, bake -> false
+ * runTime: O(n)
  */
 class _01_05_OneAway {
     boolean isOneAway(String a, String b) {
 
-        return false;
+//          2 cases.
+//          1) a.length == b.length --> replace a character, or no edits
+//          2) the difference in length is 1 --> insert/remove a character
+
+        if (Math.abs(a.length() - b.length()) > 1) {
+            return false;
+        }
+        int[] map = new int[128];
+        int countDiff = 0;
+        for (int i = 0; i < a.length(); ++i) {
+            ++map[a.charAt(i)];
+        }
+        for (int i = 0; i < b.length(); ++i) {
+            if (--map[b.charAt(i)] < 0) {
+                ++countDiff;
+            }
+        }
+        return countDiff == 0 || countDiff == 1;
     }
 
 }
--- src/main/java/arraystring/_01_06_StringCompression.java
+++ src/main/java/arraystring/_01_06_StringCompression.java
@@ -5,9 +5,27 @@ package arraystring;
  * For example, the string aabcccccaaa would become a2blc5a3.
  * If the "compressed" string would not become smaller than the original string, your method should return
  * the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ *
+ * runtime: O(n)
  */
 class _01_06_StringCompression {
     String compress(String s) {
-        throw new UnsupportedOperationException();
+        if (s == null || s.length() == 0) return s;
+        char prev = s.charAt(0);
+        int countRepetition = 1;
+        StringBuilder builder = new StringBuilder();
+        builder.append(prev);
+        for (int i = 1; i < s.length(); ++i) {
+            if (s.charAt(i) == prev) {
+                ++countRepetition;
+            } else {
+                builder.append(countRepetition);
+                builder.append(s.charAt(i));
+                prev = s.charAt(i);
+                countRepetition = 1;
+            }
+        }
+        builder.append(countRepetition);
+        return s.length() > builder.toString().length() ? builder.toString() : s;
     }
 }
