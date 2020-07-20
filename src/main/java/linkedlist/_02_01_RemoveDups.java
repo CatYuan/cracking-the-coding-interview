ade5965f27295bff87e6e89c4ef4d9efda396a9a
-g <-g>
2020-07-20 09:43:34 -0400

    linked list remove dups

--- src/main/java/linkedlist/_02_01_RemoveDups.java
+++ src/main/java/linkedlist/_02_01_RemoveDups.java
@@ -1,5 +1,9 @@
 package linkedlist;
 
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+
 /**
  * Remove Dups!
  * Write code to remove duplicates from an unsorted linked list.
@@ -7,6 +11,27 @@ package linkedlist;
 class _02_01_RemoveDups {
 
     LinkedListNode removeDups(LinkedListNode head) {
-        throw new UnsupportedOperationException();
+        if (head == null) {
+            return null;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        LinkedListNode curr = head;
+        LinkedListNode prev = null;
+        while (curr != null) {
+            if (set.contains(curr.val)) {
+                if (prev == null) {
+                    head = curr.next;
+                    curr = curr.next;
+                } else {
+                    prev.next = curr.next;
+                    curr = curr.next;
+                }
+            } else {
+                set.add(curr.val);
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+        return head;
     }
 }
